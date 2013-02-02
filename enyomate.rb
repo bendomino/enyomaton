b4e83b4d3ae848c89c918a5a5eedd264b9ab7478
Ben Di <Ben@Bens-MacBook-Air.local>
2013-02-02 14:34:00 -0500

    Rebase base_dir to app_dir for clarity

--- enyomate.rb
+++ enyomate.rb
@@ -17,36 +17,36 @@ end
 config = YAML.load_file config_file
 app_name = config["app_name"]
 namespace = config["namespace"]
-base_dir = config["base_dir"]
+app_dir = config["app_dir"]
 gap_dir = config["phonegap_dir"]
 
 # Move to the bootplate directory
-Dir.chdir "#{base_dir}/bootplate"
+Dir.chdir "#{app_dir}/bootplate"
 
 # Minify the bootplate app
 node_out = `node enyo/tools/deploy.js`
 puts node_out
 
 # Create the enyomaton directory, where we'll place our pg builds
-if not File.directory? "#{base_dir}/bootplate/enyomaton"
-	Dir.mkdir "#{base_dir}/bootplate/enyomaton"
+if not File.directory? "#{app_dir}/bootplate/enyomaton"
+	Dir.mkdir "#{app_dir}/bootplate/enyomaton"
 end
 
 # Run the phonegap create command to create the project wrapper
-if not File.directory? "#{base_dir}/bootplate/enyomaton/#{app_name}"
+if not File.directory? "#{app_dir}/bootplate/enyomaton/#{app_name}"
 	Dir.chdir "#{gap_dir}/lib/android/bin"
-	gap_out = `./create #{base_dir}/bootplate/enyomaton/#{app_name.downcase} #{namespace}.#{app_name} #{app_name}`
+	gap_out = `./create #{app_dir}/bootplate/enyomaton/#{app_name.downcase} #{namespace}.#{app_name} #{app_name}`
 	puts gap_out
 end
 
 # Move to the www directory of the Android app we just created, where our stuff needs to go
-Dir.chdir "#{base_dir}/bootplate/enyomaton/#{app_name}/assets/www"
+Dir.chdir "#{app_dir}/bootplate/enyomaton/#{app_name}/assets/www"
 
 # We'll use this to remember the cordova version- initialize for scope
 cordova_version = nil
 
 # Remove all files in the assets/www directory except our cordova lib
-Dir.foreach("#{base_dir}/bootplate/enyomaton/#{app_name}/assets/www") do |item|
+Dir.foreach("#{app_dir}/bootplate/enyomaton/#{app_name}/assets/www") do |item|
 	if not item.match(/^\.\.?$/) and not item.match(/cordova/)
 		FileUtils.rm_r item
 	elsif item.match(/cordova-(.+)\.js/)
@@ -55,11 +55,11 @@ Dir.foreach("#{base_dir}/bootplate/enyomaton/#{app_name}/assets/www") do |item|
 end
 
 # Move the deploy directory and copy its contents to the phonegap wrapper
-Dir.chdir "#{base_dir}/bootplate/deploy/bootplate"
-FileUtils.cp_r Dir.glob("*"), "#{base_dir}/bootplate/enyomaton/#{app_name}/assets/www"
+Dir.chdir "#{app_dir}/bootplate/deploy/bootplate"
+FileUtils.cp_r Dir.glob("*"), "#{app_dir}/bootplate/enyomaton/#{app_name}/assets/www"
 
 # Move the index file
-Dir.chdir "#{base_dir}/bootplate/enyomaton/#{app_name}/assets/www/"
+Dir.chdir "#{app_dir}/bootplate/enyomaton/#{app_name}/assets/www/"
 FileUtils.cp "index.html", "index.orig"
 
 new_index = File.new "index.html", "w"
